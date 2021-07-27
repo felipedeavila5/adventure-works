3c29a1795a1b69901b30949b4cc0f33c6d4a2682
lpvl98574@gmail.com <lpvl98574@gmail.com>
2021-07-27 22:26:49 +0000

    add currencyrate.sql

---
+++ models/stage/stg_currencyrate.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+with stg_currency as (
+    select  averagerate	as taxa_media,		
+            endofdayrate as taxa_fim_do_dia,		
+            modifieddate as data_moficicacao,		
+            currencyrateid	as id_taxa_moeda,		
+            tocurrencycode as id_moeda_final,		
+            currencyratedate as data_taxa_moeda,		
+            fromcurrencycode as id_moeda_inicial
+        
+    from {{ source('sales', 'currencyrate' )}}
+)
+
+select * from stg_currency
