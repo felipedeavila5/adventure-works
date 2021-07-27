8dcd8aaff4b2af049f8833d78d46d5fb36f650d3
lpvl98574@gmail.com <lpvl98574@gmail.com>
2021-07-27 22:44:54 +0000

    add stg_salesorderdetail.sql

---
+++ models/stage/stg_salesorderdetail.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+with stg_salesorderdetail as (
+    select  
+        orderqty as qtd_pedido,		
+        productid as id_produto,		
+        unitprice as preco_unitario,		
+        modifieddate as data_modificacao,		
+        salesorderid as id_pedido_vendas,		
+        specialofferid as id_oferta_especial,		
+        unitpricediscount as desconto_preco_unitario,		
+        salesorderdetailid as id_pedido_detalhe_vendas,
+        carriertrackingnumber as numero_rastreamento_operadora	
+        
+    from {{ source('sales', 'salesorderdetail' )}}
+)
+
+select * from stg_salesorderdetail
