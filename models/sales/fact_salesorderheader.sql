2e56e59346b8a8abfac16bd06b94dffe6bad9bc9
lpvl98574@gmail.com <lpvl98574@gmail.com>
2021-08-07 19:17:07 +0000

    Update: fact_salesorderheader.sql, add more columns for use on the BI filter

--- models/sales/fact_salesorderheader.sql
+++ models/sales/fact_salesorderheader.sql
@@ -10,6 +10,26 @@ stg_salesorderdetail as (
     select * from {{ref('stg_salesorderdetail')}}
 ),
 
+dim_product as (
+    select * from {{ref('dim_product')}}
+),
+
+dim_address as (
+    select * from {{ref('dim_address')}}
+),
+
+dim_credicard as (
+    select * from {{ref('dim_credicard')}}
+),
+
+dim_customer as (
+    select * from {{ref('dim_customer')}}
+),
+
+dim_salesorderheadersalesreason as (
+    select * from {{ref('dim_salesorderheadersalesreason')}}
+),
+
 transformed as(
     select 
         soh.estado,		
@@ -37,11 +57,28 @@ transformed as(
         sod.desconto_preco_unitario,		
         sod.id_pedido_detalhe_vendas,
         sod.numero_rastreamento_operadora,
-        sod.total_negociado	
+        sod.total_negociado,
+        dp.nome as nome_produto,
+        da.nome_pais,
+        da.nome_estado_provincia,
+        da.cidade,
+        dc.tipo_cartao,
+        dct.nome_completo as nome_completo_cliente
+
         
     from  stg_salesorderdetail as sod
     left join stg_salesorderheader as soh
     on (sod.id_pedido_vendas = soh.id_pedido_vendas)
+    left join dim_product as dp
+    on (sod.id_produto = dp.id_produto)
+    left join dim_address as da
+    on( soh.id_endereco_destino = da.id_endereco)
+    left join dim_credicard as dc 
+    on (soh.id_cartao_de_credito = dc.id_cartao_de_credito)
+    left join dim_customer as dct 
+    on (soh.id_cliente = dct.id_cliente)
+    left join dim_salesorderheadersalesreason as ds 
+    on (soh.id_pedido_vendas = ds.id_pedido_vendas)
 )
 
 select * from transformed
